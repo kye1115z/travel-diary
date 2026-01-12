df760e6d6c898643bbbfe2f320ba3bf0b9cbfc51
kimtaehyung <kth14200@gmail.com>
2026-01-12 15:15:53 +0900

    src/assets/components에서 src/components로 디렉토리 이동 및 TravelForm.jsx,css 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import TravelForm from "./components/TravelForm";
 function App() {
   return (
     <div className="App">
       <h1>여행 기록</h1>
+      <TravelForm/>
     </div>
   );
 }
