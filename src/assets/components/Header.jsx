f1ac86ee403794b431581d76a3723e695c8d7eed
YeEun Kim <kye1115z@naver.com>
2026-01-12 14:57:01 +0900

    feat: Header 컴포넌트 생성 및 적용

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,5 +1,24 @@
-function Header() {
-  return <></>;
+import "./Header.css";
+import { useState } from "react";
+
+function Header({ totalTrips, totalCountries }) {
+  return (
+    <header className="header">
+      <h1>✈️ 나의 여행 기록</h1>
+      <div className="data">
+        <div className="stats">
+          <div className="stat-item">
+            <div className="stat-number">{totalTrips}</div>
+            <span className="stat-label">여행지</span>
+          </div>
+          <div className="stat-item">
+            <div className="stat-number">{totalCountries}</div>
+            <span className="stat-label">여행지</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
