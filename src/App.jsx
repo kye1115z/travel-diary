9bead9ac57e1683df2975efa29829622c2d9f388
Sangmin Kang <kkang5430@naver.com>
2026-01-12 15:07:21 +0900

    Travel Card and List Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import TravelList from "./assets/components/TravelList.jsx";
 function App() {
+
+
   return (
     <div className="App">
       <h1>여행 기록</h1>
+
     </div>
   );
 }
--- src/assets/components/TravelCard.jsx
+++ src/assets/components/TravelCard.jsx
@@ -1,5 +1,39 @@
-function TravelCard() {
-  return <></>;
+function TravelCard({travel, onEdit, onDelete}) {
+  const stars = '⭐'.repeat(travel.rating);
+
+  return (
+      /*카드 이미지*/
+      <div className="travel-card">
+        <div className="card-image">
+          {travel.image ? (
+              <img src = {travel.image} alt = {travel.name}/>
+          ) : (
+              <div className="no-image">📷</div>
+          )}
+        </div>
+
+        {/*카드 내용들*/}
+        <div className="card-content">
+          <h3>{travel.name}</h3>
+          <p className="location">🎈{travel.city}</p>
+          <p className="date">📅{travel.date}</p>
+          <p className="rating">{stars}</p>
+          {travel.memo && <p className="memo">{travel.memo}</p>}
+        </div>
+
+        <div className="card-actions">
+          {/*travel 객체로 수정*/}
+          <button className="btn-edit" onClick={() => onEdit(travel)}>
+            수정
+          </button>
+
+          {/* id 로 카드 삭제*/}
+          <button className="btn-delete" onClick={() => onDelete(travel.id)}>
+            삭제
+          </button>
+        </div>
+      </div>
+  );
 }
 
 export default TravelCard;
